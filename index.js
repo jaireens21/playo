60d75277131c33c661aa53d62fc54c65686188e0
Jaireen Singh <jaireen.s21@gmail.com>
2021-12-03 15:10:30 -0500

    added home page

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express=require('express');
 const app=express();
+const path=require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", (req,res)=>{
     res.send("hello from playo");
+})
+app.get("/home", (req,res)=>{
+    res.render('home');
 })
 
 
